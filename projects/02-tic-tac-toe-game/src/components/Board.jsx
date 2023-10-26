b027c77719bc514d866bffa90671eb3e11e06a1a
LPDev93 <lparodi.dev@gmail.com>
2023-10-26 15:33:50 -0500

    Update

--- projects/02-tic-tac-toe-game/src/components/Board.jsx
+++ projects/02-tic-tac-toe-game/src/components/Board.jsx
@@ -1,6 +1,6 @@
 import { Square } from "./Square";
 
-export function Board({board, updateBoard}) {
+export function Board({ board, updateBoard }) {
   return board.map((cell, index) => {
     return (
       <Square key={index} index={index} updateBoard={updateBoard}>
